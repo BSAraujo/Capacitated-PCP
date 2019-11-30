3e83ce7b065c44e48022bd4660ccc58dd22367c8
Italo Gomes Santana <italo.gomes.santana@gmail.com>
2019-11-30 09:52:42 -0300

    All experiment results; Two excel files: a raw CSV and another including filters;

--- experiments/script_local.sh
+++ experiments/script_local.sh
@@ -1,13 +1,31 @@
 
 
-instances='../instances/S1'/*
-for folder in $instances
+# instances='../instances/S1'/*
+# for folder in $instances
+# do
+#     for instance in $folder/*
+#     do
+#         echo "Executing: $instance"
+#         basename_instance=${instance##*/}
+#         julia ../src/Main.jl $instance > output-$basename_instance.txt
+#     done
+# done
+instances=('../instances/p66200/p66200-37.txt' '../instances/p66200/p66200-38.txt' '../instances/p66200/p66200-39.txt' '../instances/p66200/p66200-40.txt')
+for instance in "${instances[@]}"
 do
-    for instance in $folder/*
-    do
         echo "Executing: $instance"
         basename_instance=${instance##*/}
+        # echo $instance
+        # echo $basename_instance
         julia ../src/Main.jl $instance > output-$basename_instance.txt
-    done
 done
 
+
+for instance in "../instances/S1/p80200"/*
+do
+        echo "Executing: $instance"
+        basename_instance=${instance##*/}
+        # echo $instance
+        # echo $basename_instance
+        julia ../src/Main.jl $instance > output-$basename_instance.txt
+done
