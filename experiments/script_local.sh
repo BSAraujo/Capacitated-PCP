04ba82405bf7946d2d40e3463e9a190c04217b3f
Italo Gomes Santana <italo.gomes.santana@gmail.com>
2019-12-01 14:55:24 -0300

    remaining executions;

--- experiments/script_local.sh
+++ experiments/script_local.sh
@@ -10,22 +10,23 @@
 #         julia ../src/Main.jl $instance > output-$basename_instance.txt
 #     done
 # done
-instances=('../instances/p66200/p66200-37.txt' '../instances/p66200/p66200-38.txt' '../instances/p66200/p66200-39.txt' '../instances/p66200/p66200-40.txt')
+instances=('../instances/S1/p66200/p66200-37.txt' '../instances/S1/p66200/p66200-38.txt' '../instances/S1/p66200/p66200-39.txt' '../instances/S1/p66200/p66200-40.txt')
 for instance in "${instances[@]}"
 do
         echo "Executing: $instance"
         basename_instance=${instance##*/}
         # echo $instance
         # echo $basename_instance
+        # cat $instance
         julia ../src/Main.jl $instance > output-$basename_instance.txt
 done
 
 
-for instance in "../instances/S1/p80200"/*
-do
-        echo "Executing: $instance"
-        basename_instance=${instance##*/}
-        # echo $instance
-        # echo $basename_instance
-        julia ../src/Main.jl $instance > output-$basename_instance.txt
-done
+# for instance in "../instances/S1/p80200"/*
+# do
+#         echo "Executing: $instance"
+#         basename_instance=${instance##*/}
+#         # echo $instance
+#         # echo $basename_instance
+#         julia ../src/Main.jl $instance > output-$basename_instance.txt
+# done
